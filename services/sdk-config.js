1d0f70aa46edbdb0b1063e157b833146e25baf87
limingtech <limingtech@hotmail.com>
2017-05-16 10:03:12 +0800

    first commit

---
+++ services/sdk-config.js
@@ -0,0 +1,22 @@
+var Vue=require('vue')
+var errorMessage=require('lm-se-error-message')
+var calcUriParams=require('lm-ut-calc-uri-params')
+
+module.exports=function(success,fail){
+
+	Vue.http.get('/rest/json.jhtml?req=' + JSON.stringify(
+		methodName:'QueryJssdkConfig',
+		urlInfo:location.href.split('#')[0],
+		wechatOpenId:calcUriParams.getUriQuery().state
+	})).then((response)=>{
+		let data=JSON.parse(response.data)
+		if(data.resultCode==100){
+			success && success(data)
+		}else{
+			fail && fail(errorMessage[data.resultCode])
+		}
+	},(response)=>{
+		fail && fail('网络错误')
+	})
+}
+
