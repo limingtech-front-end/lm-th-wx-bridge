ce7acd6a10d755ee6b0e3c1fcfff732879fae706
limingtech <limingtech@hotmail.com>
2017-05-16 10:47:53 +0800

    fix bugs

--- services/sdk-config.js
+++ services/sdk-config.js
@@ -3,12 +3,14 @@ var errorMessage=require('lm-sr-error-message')
 var calcUriParams=require('lm-ut-calc-uri-params')
 
 module.exports=function(success,fail){
-
-	Vue.http.get('/rest/json.jhtml?req=' + JSON.stringify({
-		methodName:'QueryJssdkConfig',
-		urlInfo:location.href.split('#')[0],
-		wechatOpenId:calcUriParams.getUriQuery().state
-	})).then((response)=>{
+	Vue.http.get('/rest/json.jhtml',{
+			params:{
+				req:JSON.stringify({
+					methodName:'QueryJssdkConfig',
+					urlInfo:location.href.split('#')[0],
+					wechatOpenId:calcUriParams.getUriQuery().state})
+			}
+		}).then((response)=>{
 		let data=JSON.parse(response.data)
 		if(data.resultCode==100){
 			success && success(data)
