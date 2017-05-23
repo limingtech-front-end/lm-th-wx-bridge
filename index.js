a9da951217d3208936a0e4bf597c908dd9e3bb22
limingtech <limingtech@hotmail.com>
2017-05-23 20:18:12 +0800

    fix bugs

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function initWechatWebView() {
 	return new Promise(function(resolve,reject){
 		if(window.wx){
 			console.log('wx global object detected')
-			sdkConfig((config)=>{
+			sdkConfig(function(config){
 				console.log('wechat config settings getted')
 				wx.config({
 					 // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
